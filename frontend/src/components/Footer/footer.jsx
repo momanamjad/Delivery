d5e1a0bdcdd9278ae76a0820260f52fefacc002b
momanamjad <momanamjad07@gmail.com>
2025-09-26 14:56:34 +0500

    mm

--- frontend/src/components/Footer/footer.jsx
+++ frontend/src/components/Footer/footer.jsx
@@ -32,6 +32,7 @@ const footer = () => {
             <ul>
                 <li>+923281109353</li>
                 <li>momanamjad07@gmail.com</li>
+                <li>contsct me</li>
             </ul>
         </div>
 
