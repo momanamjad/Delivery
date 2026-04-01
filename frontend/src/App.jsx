981ba54c3d0a6fd3c18b10f65c0db0814b73334b
Moman Amjad <momanamjad07@gmail.com>
2026-04-01 11:23:42 +0500

    test:vercel error resolve

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Navbar from "./components/Navbar/navbar";
+import Navbar from "./components/Navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
 import  Home from './pages/Home/home'
 import Cart from './pages/Cart/cart'
@@ -17,7 +17,7 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Home/>} />
             <Route path='/cart' element={<Cart/>}/>
-            
+
             <Route path='/order'element={<Place_order/>}/>
         </Routes>
       </div>
